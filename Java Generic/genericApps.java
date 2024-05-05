262e119bf8206dfe1a9ea4e7a0f13152f50b7728
Risky Pandu <riskyypanduu@gmail.com>
2024-05-05 19:40:35 +0700

    pengumpulan tugas

--- Java Generic/genericApps.java
+++
@@ -1,9 +0,0 @@
-package generic;
-
-public class genericApps {
-    public static void main(String[] args) {
-        
-        genericClass <Integer> dataKu = new genericClass<> (7);
-        System.out.println(dataKu.getData());
-    }
-}
