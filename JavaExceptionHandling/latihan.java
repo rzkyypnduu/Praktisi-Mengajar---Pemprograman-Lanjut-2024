d3f3f0ab51145ca618b9fbf40c3587ba499bdcf5
Risky Pandu <riskyypanduu@gmail.com>
2024-05-15 14:23:32 +0700

    contoh

--- JavaExceptionHandling/latihan.java
+++ JavaExceptionHandling/latihan.java
@@ -1,5 +1,19 @@
 package JavaExceptionHandling;
 
 public class latihan {
+    public static void main(String[] args) {
+        
+        // int angka = 10/0;
+        // System.out.println(angka);
+        
+        try {
+            int angka = 10/0;
+            System.out.println(angka);
+        } catch (ArithmeticException a) {
+            // TODO: handle exception
+            System.out.println("Terjadi Kesalahan :" + a);
+
+        }
+    }
     
 }
